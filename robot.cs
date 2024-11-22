8dc4b6d1f8bd14ddfb32fea82a2b8c59c1ea9e2a
Raevinc <acaarumahlatu@gmail.com>
2024-11-22 14:11:29 +0700

    Add files via upload

---
+++ robot.cs
@@ -0,0 +1,19 @@
+namespace latihanobjek
+{
+    class Robot
+    {
+        //Attribute / variable
+        public string nama;
+        public int jumlahkaki;
+        public int jumlahtangan;
+        public int jumlahrocket;
+        public int jumlahsenjata;
+
+        //Method
+        public void Jalan()
+        {
+            Console.WriteLine()
+             
+        }
+    }
+}
